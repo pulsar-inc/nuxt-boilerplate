e8dadcb40592d017a873229bc95e69b718de7391
Arnaud Gissinger <agissing@student.42.fr>
2020-05-09 12:00:22 +0200

    [ADD] color theme
    - theme switcher
    - scss trick for using css variables

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="bg-bg">
+  <div id="main-container" :class="{'dark-theme': !pastelTheme, 'pastel-theme': pastelTheme}">
+    <svg-icon v-if="pastelTheme" id="theme-switcher" name="halloween" @click="pastelTheme = false" />
+    <svg-icon v-else id="theme-switcher" name="moon" @click="pastelTheme = true" />
     <nuxt />
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data: () => ({
+    pastelTheme: false
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+  #theme-switcher {
+    position: absolute;
+    right: 0;
+    height: 32px;
+    width: 32px;
+    margin: 10px;
+  }
 </style>
